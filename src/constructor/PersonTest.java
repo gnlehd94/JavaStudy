539a755d892a2a6125cc5d617f73f6dd7b6b32a4
whidong <gnlehd94@naver.com>
2022-04-04 13:38:39 +0900

    Implementation of test class

--- src/constructor/PersonTest.java
+++ src/constructor/PersonTest.java
@@ -3,7 +3,12 @@ package constructor;
 public class PersonTest {
 
 	public static void main(String[] args) {
-		Person personLee = new Person();
+		Person personKim = new Person();
+		personKim.name = "김유신";
+		personKim.weight = 85.5F;
+		personKim.height = 180.0F;
+		
+		Person personLee = new Person("이순신", 175, 75);
 	}
 
 }
