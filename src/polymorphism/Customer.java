d4e4bc2e1fb89bd8669394faf874f2d2f068fe39
whidong <gnlehd94@naver.com>
2022-05-09 18:21:58 +0900

    새로운 고객 등급 추가

--- src/polymorphism/Customer.java
+++ src/polymorphism/Customer.java
@@ -1,25 +1,39 @@
 package polymorphism;
 
+
 public class Customer {
+	
 	protected int customerID;
 	protected String customerName;
 	protected String customerGrade;
 	int bonusPoint;
 	double bonusRatio;
 	
-	public Customer() {
+	public Customer()
+	{
 		initCustomer();
 	}
-	
-	public Customer(int customerID, String customerName) {
+
+	public Customer(int customerID, String customerName){
 		this.customerID = customerID;
 		this.customerName = customerName;
+		
 		initCustomer();
 	}
 	
-	private void initCustomer() {
+	private void initCustomer()
+	{
 		customerGrade = "SILVER";
-		bonusRatio = 0.01;
+		bonusRatio = 0.01;	
+	}
+	
+	public int calcPrice(int price){
+		bonusPoint += price * bonusRatio;
+		return price;
+	}
+	
+	public String showCustomerInfo(){
+		return customerName + " 님의 등급은 " + customerGrade + "이며, 보너스 포인트는 " + bonusPoint + "점입니다.";  
 	}
 	
 	public int getCustomerID() {
@@ -45,15 +59,4 @@ public class Customer {
 	public void setCustomerGrade(String customerGrade) {
 		this.customerGrade = customerGrade;
 	}
-
-	public int calcPrice(int price) {
-		bonusPoint += price * bonusRatio;
-		return price;
-	}
-	
-	public String showCustomerInfo() {
-		return customerName + " 님의 등급은 " + customerGrade + "이며, 보너스 포인트는" +
-	bonusPoint + "입니다.";
-	}
-
 }
